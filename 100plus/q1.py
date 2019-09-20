11e65941c92c8612d83181076868ed6dd339d3cc
Ashwin Jayaprakash <fanbyprinciple@gmail.com>
2019-09-20 20:59:34 +0530

    100plus: day 2

--- 100plus/q1.py
+++ 100plus/q1.py
@@ -1,11 +1,18 @@
+# my solution
+number= input('Enter a number : ')
+ans=1
+for i in range(1,int(number)+1):
+    ans=ans*i
+print(ans)
 
-for i in range(2000,3201):
-    if(i%7==0):
-        if(i%5==0):
-            continue
-        else:
-            print(i+",",end="")
+""" proposed solution
 
+def fact(x):
+    if(x==0):
+        return x
+    else:
+        return x * fact(x -1)
+x = input("enter a number: ")
+print(fact(x))
 
-            
-    
+"""

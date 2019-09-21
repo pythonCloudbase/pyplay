5d26b7cd2530f66ff7212bf84047a7c0cb5a0341
Ashwin Jayaprakash <fanbyprinciple@gmail.com>
2019-09-21 16:24:02 +0530

    100plus: Day 3

--- 100plus/q1.py
+++ 100plus/q1.py
@@ -1,14 +1,4 @@
-# my solution
-number= input('Enter a number : ')
-ans=1
-for i in range(1,int(number)+1):
-    ans=ans*i
-print(ans)
-
-""" proposed solution
-
-def fact(x):
-    if(x==0):
+ if(x==0):
         return x
     else:
         return x * fact(x -1)
--- 100plus/rough_work.py
+++ 100plus/rough_work.py
@@ -1,11 +1,13 @@
+''' # my solution
+get = input("Enter a number: ")
+dict ={}
 
-for i in range(2000,3201):
-    if(i%7==0):
-        if(i%5==0):
-            continue
-        else:
-            print(i+",",end="")
+for i in range(1,int(get)+1):
+    dict[i] = i*i
 
+print(dict)
+
+'''
 
             
     

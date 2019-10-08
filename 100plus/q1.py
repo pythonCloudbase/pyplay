3b1e43d9582a03e1c16e61822a4a0d66b2c43b03
Ashwin Jayaprakash <fanbyprinciple@gmail.com>
2019-10-09 01:13:58 +0530

    100plus: 12

--- 100plus/q1.py
+++ 100plus/q1.py
@@ -1,8 +1,15 @@
-get = input("Enter  number sequence: ").split(',')
+import math
+iter = []
+for i in range (1000,30001):
+    c = i
+    flag = True
+    while(c>1):
+        if((c%10)%2 != 0):
+            flag = False
+        c = math.floor(c/10)
+        
 
-out = []
-for i in get:
-    if(int(i)%5 == 0):
-        out.append(i)
+    if(flag):
+        iter.append(str(i))
 
-print(",".join(out))
+print(",".join(iter))

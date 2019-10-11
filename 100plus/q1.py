3c38ec0ab144dab49b41a592e13bcb7bb361bfe3
Ashwin Jayaprakash <fanbyprinciple@gmail.com>
2019-10-11 08:02:54 +0530

    100plus : 15

--- 100plus/q1.py
+++ 100plus/q1.py
@@ -1,16 +1,3 @@
-get = input( "Enter thre phrase: " )
+get = int(input("Enter a number : "))
 
-upper = 0
-lower = 0
-
-for i in get :
-    if(i.isupper()):
-        upper += 1
-    elif(i.islower()):
-        lower += 1
-
-print("UPPER ", upper)
-print("LOWER ", lower)
-
-
-
+print(get + get * 10 + get + get * 100 + get * 10 + get + get * 1000 + get * 100 + get * 10 + get)

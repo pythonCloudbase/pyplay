569357017df5207be44284df7cc3586a009c2bcd
Ashwin Jayaprakash <fanbyprinciple@gmail.com>
2019-10-11 07:59:38 +0530

    100plus : 14

--- 100plus/q1.py
+++ 100plus/q1.py
@@ -1,21 +1,16 @@
-get = input("Enter the phrase :")
-#print(get)
+get = input( "Enter thre phrase: " )
 
-letters = 0
-numbers = 0
+upper = 0
+lower = 0
 
-for i in get:
+for i in get :
+    if(i.isupper()):
+        upper += 1
+    elif(i.islower()):
+        lower += 1
 
-    # if (i in (range(ord('a'), ord('z')) or range(ord('A'), ord():
-    #     letter += 1
-    # elif(i in range(0,10)):
-    #     numbers += 1
+print("UPPER ", upper)
+print("LOWER ", lower)
 
-    #print(i)
-    if(i.isalpha()):
-        letters += 1
-    elif(i.isnumeric()):
-        numbers += 1
 
-print("LETTERS ", letters)
-#("NUMBERS ", numbers)
+

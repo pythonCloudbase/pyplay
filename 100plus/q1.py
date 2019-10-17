7514e2873deedae26239cca4183709344e41b9c2
Ashwin Jayaprakash <fanbyprinciple@gmail.com>
2019-10-17 23:52:36 +0530

    100plus; nineteen part one

--- 100plus/q1.py
+++ 100plus/q1.py
@@ -1,19 +1,5 @@
-get = input("Enter the passwords: ").split(',')
+import sys
 
-goodpass = []
-for i in get :
-    passed = 0
-    if (len(i) > 5 & len(i) < 13):
-        for j in i:
-            if(j.isupper()):
-                passed += 1
-            if(j.islower()):
-                passed += 1
-            if(j.isnumeric()):
-                passed += 1
-            if(j in ['$','#','@']):
-                passed += 1
-    if(passed > 3):
-        goodpass.append(i)
+get= sys.stdin.readlines()
 
-print(",".join(goodpass))
+print(get)

4ae8f501f5a3659883f69cfc660dede3b89709c3
Ashwin Jayaprakash <fanbyprinciple@gmail.com>
2019-10-18 00:38:27 +0530

    100plus: ninetwwn

--- 100plus/q1.py
+++ 100plus/q1.py
@@ -2,4 +2,42 @@ import sys
 
 get= sys.stdin.readlines()
 
-print(get)
+sortScoreList = []
+
+for line in get :
+    totalscore = 0
+    
+    name,age,score = line.strip().split(',')
+    #print(name,age,score)
+
+    d = 1
+    for char in name:
+        totalscore += ord(char) * 100000/d
+        d = d * 10
+
+    totalscore += int(age) + int(score)
+    sortScoreList.append(totalscore)
+
+
+#print(sortScoreList)
+
+
+sortIndex = []
+for i in range(len(sortScoreList)):
+    min = 0
+
+    for j in range(len(sortScoreList)):
+        print(min,j)
+        if (sortScoreList[min] > sortScoreList[j]):
+            
+            min = j
+    sortIndex.append(min)
+    sortScoreList[min] = 1000000000000000000
+    #print(sortScoreList, sortIndex)
+
+#print(sortIndex)
+
+
+for i in sortIndex:
+    print((get[i].strip()))
+

102704d9cc9048122f7932c0015559dea086f409
Ashwin Jayaprakash <fanbyprinciple@gmail.com>
2019-10-18 23:19:39 +0530

    100plus : twenty

--- 100plus/q1.py
+++ 100plus/q1.py
@@ -1,43 +1,6 @@
-import sys
+get = input("Input range : ").split(',')
 
-get= sys.stdin.readlines()
-
-sortScoreList = []
-
-for line in get :
-    totalscore = 0
-    
-    name,age,score = line.strip().split(',')
-    #print(name,age,score)
-
-    d = 1
-    for char in name:
-        totalscore += ord(char) * 100000/d
-        d = d * 10
-
-    totalscore += int(age) + int(score)
-    sortScoreList.append(totalscore)
-
-
-#print(sortScoreList)
-
-
-sortIndex = []
-for i in range(len(sortScoreList)):
-    min = 0
-
-    for j in range(len(sortScoreList)):
-        print(min,j)
-        if (sortScoreList[min] > sortScoreList[j]):
-            
-            min = j
-    sortIndex.append(min)
-    sortScoreList[min] = 1000000000000000000
-    #print(sortScoreList, sortIndex)
-
-#print(sortIndex)
-
-
-for i in sortIndex:
-    print((get[i].strip()))
+for i in range(int(get[0]), int(get[1])):
+    if(i%7 == 0):
+        print(i)
 

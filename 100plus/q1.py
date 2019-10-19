874abb02bde9d5638e5a1ee59c81e468e221c030
Ashwin Jayaprakash <fanbyprinciple@gmail.com>
2019-10-19 23:35:57 +0530

    100 plus : twenty one

--- 100plus/q1.py
+++ 100plus/q1.py
@@ -1,6 +1,27 @@
-get = input("Input range : ").split(',')
+import sys
+import math
 
-for i in range(int(get[0]), int(get[1])):
-    if(i%7 == 0):
-        print(i)
+get = sys.stdin.readlines()
+
+x = 0
+y = 0
+
+for action in get:
+    direction = action.strip('\n').split(' ')[0].lower()
+    steps = action.strip('\n').split(' ')[1]
+
+    if (direction == 'up'):
+        y -= int(steps)
+    elif (direction == 'down'):
+        y += int(steps)
+    elif (direction == 'left'):
+        x -= int(steps)
+    elif(direction == 'right'):
+        x += int(steps)
+    else:
+        pass
+
+
+print(math.floor(math.sqrt(x**2 + y**2)))
+
 

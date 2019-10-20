54fe59153d04572069cb80d9cacd700feaf114e8
Ashwin Jayaprakash <fanbyprinciple@gmail.com>
2019-10-20 12:28:49 +0530

    100plus :  twenty two

--- 100plus/q1.py
+++ 100plus/q1.py
@@ -1,27 +1,14 @@
-import sys
-import math
+get = input("Enter the sentence : ").split(" ")
+unique = list(set(get))
+unique.sort()
 
-get = sys.stdin.readlines()
+diction = {}
 
-x = 0
-y = 0
+for i in unique :
+    diction[i] = 0
 
-for action in get:
-    direction = action.strip('\n').split(' ')[0].lower()
-    steps = action.strip('\n').split(' ')[1]
+for j in get:
+    diction[j] += 1
 
-    if (direction == 'up'):
-        y -= int(steps)
-    elif (direction == 'down'):
-        y += int(steps)
-    elif (direction == 'left'):
-        x -= int(steps)
-    elif(direction == 'right'):
-        x += int(steps)
-    else:
-        pass
-
-
-print(math.floor(math.sqrt(x**2 + y**2)))
-
-
+for z in diction.keys():
+    print(z, ':', diction[z])

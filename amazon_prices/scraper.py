ddc780df09b5951cefd9cd7a11f373660c17df36
Ashwin Jayaprakash <fanbyprinciple@gmail.com>
2019-11-23 15:22:22 +0530

    amazon_prices : email added

--- amazon_prices/scraper.py
+++ amazon_prices/scraper.py
@@ -1,8 +1,30 @@
 import requests 
 from bs4 import BeautifulSoup
+import smtplib
+
+import time
 
 URL = 'https://www.amazon.in/Wacom-CTL-472-6-inch-3-5-inch-Graphic/dp/B078HRR1XV/ref=sr_1_3?keywords=Wacom+1+472&qid=1574496026&s=computers&sr=1-3'
 headers = {"User-Agent" : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:70.0) Gecko/20100101 Firefox/70.0'}
+
+def send_mail():
+    try:
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.ehlo()
+        server.starttls()
+        server.ehlo()
+
+        server.login('depends@onyouremail.com', '8charpassword')
+        body = 'check the link : ' + URL
+        msg = f"Subject: {subject}\n\n{body}"
+
+        server.sendmail("from@gmail.com", "toyour@, msggmail.com", msg)
+        print("Hey mail sent")
+
+        server.quit()
+    
+    except:
+        print("Configure mail correctly before sending.")
 
 
 def check_price():
@@ -17,5 +39,11 @@ def check_price():
     fprice = float( "".join(without_comma))
     print(title, " : ", fprice)
 
-check_price()
+    if(fprice <  3500):
+        send_mail()
+
+
+while(True):
+    check_price()
+    time.sleep(3600)
 

ce19a133a5693cf1901f841c83b29aa7b41beb5c
Ashwin Jayaprakash <fanbyprinciple@gmail.com>
2020-01-02 00:31:52 +0530

    classic_problems : REEa

--- classic_problems/trivial_compression.py
+++ classic_problems/trivial_compression.py
@@ -1,3 +1,20 @@
 class CompressedGene:
     def __init__(self, gene):
         self.compress(gene)
+    
+    def compress(self, gene):
+
+        self.bit_string <<=2
+        if nucleotide == "A":
+            self.bit_string |= 0b00
+        
+        elif nucleotide == "C":
+            self.bit_String |= 0b01
+        
+        elif nucleotide == "G":
+            self.bit_string |= 0b10
+        
+        elif nucleotide == "T":
+            self.bit_string |= 0b11
+        else:
+            raise ValueError("Invalid Nucleotide:{}".form)

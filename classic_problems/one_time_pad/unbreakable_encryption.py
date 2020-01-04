db113ee09e2aa2ce0fdb60a3a8c79b817f3dca38
Ashwin Jayaprakash <fanbyprinciple@gmail.com>
2020-01-04 19:43:15 +0530

    calssic_problems : trivial compression done

---
+++ classic_problems/one_time_pad/unbreakable_encryption.py
@@ -0,0 +1,23 @@
+from secrets import token_bytes
+from typing import Tuple
+
+def random_key(length):
+    tb = token_bytes(length)
+    return int.from_bytes(tb, "big") # big indian format
+
+def encrypt(original):
+    original_bytes = original.encode()
+    dummy = random_key(len(original_bytes))
+    original_key = int.frombytes(original_bytes, "big")
+    encrypted = original_key ^ dummy
+    return dummy, encrypted
+
+def decrypt(key1, key2):
+    decrypted = key1 ^ key2
+    temp = decrypted.to_bytes((decrypted.bit_length() + 7) // 8, "big")
+    return temp.decode()
+
+if __name__ == "__main__":
+    key1, key2 = encrypt("One time Pad! ")
+    result = decrypt(key1, key2)
+    print(result)

18772dfc6e67da48dc35f23a1a90d887a94fd876
Ashwin Jayaprakash <fanbyprinciple@gmail.com>
2020-06-08 12:37:11 +0530

    Update github_searcher.py

--- github_searcher/github_searcher.py
+++ github_searcher/github_searcher.py
@@ -6,8 +6,8 @@ if (get==""):
     get = "fanbyprinciple"
 
 url= "https://api.github.com/users/" + get + "/following"
-HEADERS = {'Authorization':'fanbyprinciple: d263fa634a5c13bce9eb7dea272d01e6573232ed '}
-PARAMS = {'fanbyprinciple':  "d263fa634a5c13bce9eb7dea272d01e6573232ed" }
+# add your token here as PARAMS
+# PARAMS = {username: token}
 # headers={"content-type":"application/json", "Accept-Charset":"UTF-8"}
 
 r = requests.get(url, params=PARAMS)

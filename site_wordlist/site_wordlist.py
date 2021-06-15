b7677e5774222d21f109cf97569b209231381e14
fanbyprinciple <fanbyprinciple@gmail.com>
2021-06-15 16:35:04 +0530

    added more code

--- site_wordlist/site_wordlist.py
+++ site_wordlist/site_wordlist.py
@@ -3,9 +3,7 @@ import requests as r
 from bs4 import BeautifulSoup as bs
 import argparse as ap
 
-# url = "https://www.nadra.gov.pk/?__cf_chl_jschl_tk__=e17ee01c3e1c4ba36ed9b44e3764ba861fe7a61c-1623383234-0-AUfe73hDRG-fLoC0hHYwI1otBKxHKEOUpe2O7TlA1jw765tynkm_EmC9Ye7BrJhAw9gDUQiAerIzotBgUNuyeangI2DxXSdKaydoo2JLlYN699O6SPIPFm82fbs7uIeP5pMxsD6xeo9D4oCN6N4umdAPNHER4l6d8eAygpF4vLUz9F3oan-gfd8W3Gxt7npLLSVWfiFSo4OisI643uGNlbTNUtcbRgIaiaib8ROaV-1Q3zxUS-lit3TvXQPogoWUPLwXwttYKrBdn-GRkipkfhdGRsl-u31Rf6w_LleyLqKFdmq-bd0PiDONOppv7UW8ENzSmhT9YBNS4YaH3XbvQhTtpIg05x9A_JqQFR2kQck8rf4PDz7yEx5gwyTsFkgEqKC9brJg8sCcSAuZMr6ifEY"
-url = "https://www.paknavy.gov.pk/"
-url = "https://www.bahria.edu.pk/office-directory/"
+
 req = r.get(url, verify=True)
 
 html = req.text

cabefadb370a9a3230b5cc376f2a64d03101d024
fanbyprinciple <fanbyprinciple@gmail.com>
2021-06-15 16:21:23 +0530

    added more code

--- site_wordlist/google_search_results.py
+++ site_wordlist/google_search_results.py
@@ -7,11 +7,31 @@ driver = webdriver.Chrome(executable_path="C:\\Users\\HP\\Downloads\\chromedrive
 
 driver.get("http://www.google.com")
 element = driver.find_element_by_name("q")
-element.send_keys("Spring MVC Login form Example")
+
+element.send_keys(input("Enter your search query? "))
 element.submit()
 
-results = driver.find_elements_by_xpath("//div[@class='g']//div[@class='r']//a[not(@class)]")
 
-for result in results:
-    print(result)
+results = driver.find_elements_by_xpath("//*[@id='rso']//*[@class='g']//div//div//div[1]//a")
+# //*[@id="rso"]/div[2]/div/div/div[1]/a
+# for x in dir(results[0]):
+#     print(x)
+
+top_results = []
+
+
+for result in results: 
+    link = result.get_attribute("href")
+    
+    top_results.append(link)
+
+
     # print(result.get_attribute("href"))
+
+for link in top_results:
+    driver.get(link)
+    driver.save_screenshot("./image/" + str(link.split("/")[2]) + ".png")
+    # driver.quit()
+
+
+driver.close()

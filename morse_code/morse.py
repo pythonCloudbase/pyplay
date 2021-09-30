9308b21e49094ae1fe637b6292c2b0fe8b30b64f
Ashwin Jayaprakash <fanbyprinciple@gmail.com>
2021-09-30 10:55:09 +0530

    morse code added

---
+++ morse_code/morse.py
@@ -0,0 +1,49 @@
+try:
+    import winsound
+except ImportError:
+    import os
+    def playsound(frequency,duration):
+        #apt-get install beep
+        os.system('beep -f %s -l %s' % (frequency,duration))
+else:
+    def playsound(frequency,duration):
+        winsound.Beep(frequency,duration)
+
+import time
+
+MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
+                    'C':'-.-.', 'D':'-..', 'E':'.',
+                    'F':'..-.', 'G':'--.', 'H':'....',
+                    'I':'..', 'J':'.---', 'K':'-.-',
+                    'L':'.-..', 'M':'--', 'N':'-.',
+                    'O':'---', 'P':'.--.', 'Q':'--.-',
+                    'R':'.-.', 'S':'...', 'T':'-',
+                    'U':'..-', 'V':'...-', 'W':'.--',
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+                    '1':'.----', '2':'..---', '3':'...--',
+                    '4':'....-', '5':'.....', '6':'-....',
+                    '7':'--...', '8':'---..', '9':'----.',
+                    '0':'-----', ', ':'--..--', '.':'.-.-.-',
+                    '?':'..--..', '/':'-..-.', '-':'-....-',
+                    '(':'-.--.', ')':'-.--.-'}
+
+
+file1 = open('new_input.txt', 'r+')
+content = file1.readlines()
+
+output_text = ""
+
+for f in content:
+    for w in f:
+        if w.upper() in MORSE_CODE_DICT.keys():
+            print(MORSE_CODE_DICT[w.upper()])
+            output_text += MORSE_CODE_DICT[w.upper()]
+
+print(output_text)
+
+for i in output_text:
+    if (i=='.'):
+        playsound(600,100)
+    else :
+        playsound(500,200)
+    time.sleep(0.1)
